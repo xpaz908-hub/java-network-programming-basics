89bdefa7f70d907506e5bf8c86ad54b2511f9df5
xpaz908 <xpaz908@gmail.com>
2026-06-09 23:30:39 -0400

    half-duplex communication

--- client-server-chat/Client.java
+++ client-server-chat/Client.java
@@ -4,34 +4,38 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        // Clear the terminal and initialize resources for the client
         Util.clearTerminal();
 
         try (Scanner scan = new Scanner(System.in);
-             // Attempt to connect to the server at localhost on port 80
+            // Attempt to connect to the server at localhost on port 80
             Socket socket = new Socket("localhost", 80);
             // Set up input and output streams for communication with the client
-            BufferedReader bufferedReader = new BufferedReader(
+            BufferedReader reader = new BufferedReader(
                 new InputStreamReader(socket.getInputStream()));
-            BufferedWriter bufferedWriter = new BufferedWriter(
-                new OutputStreamWriter(socket.getOutputStream()))) {
-            
+            BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream()))
+        ) {
             System.out.println("Connected to server: " + socket.getInetAddress());
             System.out.println(Util.bold("Ready to chat!\n"));
+            String serverMsg;
 
+            // Enter an infinite loop to communicate with the server
             while (true) {
                 System.out.print("-> ");
-                bufferedWriter.write(scan.nextLine());
-                bufferedWriter.newLine();
-                bufferedWriter.flush();
-                
-                // String msg = bufferedReader.readLine();
-                // if(msg != null)
-                //     System.out.println("\nServer: " + msg);
-                // else
-                //     break;
+                writer.write(scan.nextLine());
+                writer.newLine();
+                writer.flush();
+
+                if ((serverMsg = reader.readLine()) != null)
+                    System.out.println("\nServer: " + serverMsg);
             }
-        } catch (Exception e) {
+        } catch (ConnectException ce) {
+            System.out.println("Unable to connect to server. Make sure the server is running.");
+        }
+        catch (SocketException se) {
+            System.out.println("Server disconnected.");
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
--- client-server-chat/Server.java
+++ client-server-chat/Server.java
@@ -4,42 +4,45 @@ import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
-        // Clear the terminal and indicate that the server is listening on port 80
-        Util.clearTerminal();
-        // Initialize server socket and other resources
-        System.out.println("Server is listening on port 80...");
-
-        try (Scanner scan = new Scanner(System.in);
-            // Create a server socket on port 80 (listens for incoming connections)
-            ServerSocket serverSocket = new ServerSocket(80);
-            // Wait for a client to connect and block until a connection is made.
-            // When a connection is made, returns a new socket for communication with the client
-            Socket socket = serverSocket.accept();
-            // Set up input and output streams for communication with the client
-            BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
-            BufferedWriter bufferedWriter = new BufferedWriter(
-                new OutputStreamWriter(socket.getOutputStream()))) {
-            
+        // Enter an infinite loop to continuously accept client connections
+        while (true) { 
             Util.clearTerminal();
-            System.out.println("Client connected: " + socket.getInetAddress());
-            System.out.println(Util.bold("Ready to chat!\n"));
+            System.out.println("Server is listening on port 80...");
 
-            // Enter an infinite loop to communicate with the client
-            while (true) {
+            try (Scanner scan = new Scanner(System.in);
+                // Create a server socket on port 80 (listens for incoming connections)
+                ServerSocket serverSocket = new ServerSocket(80);
+                // Wait for a client to connect and block until a connection is made.
+                // When a connection is made, returns a new socket for communication with the client
+                Socket socket = serverSocket.accept();
+                // Set up input and output streams for communication with the client
+                BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream()))
+            ) {
+                Util.clearTerminal();
+                System.out.println("Client connected: " + socket.getInetAddress());
+                System.out.println(Util.bold("Ready to chat!\n"));
                 System.out.print("-> ");
-                String msg = bufferedReader.readLine();
-                if(msg != null)
-                    System.out.println("\nClient: " + msg);
-                else
-                    break;
-
-                // bufferedWriter.write(scan.nextLine()); 
-                // bufferedWriter.newLine();
-                // bufferedWriter.flush();
+                String clientMsg;
+                
+                // Enter an infinite loop to communicate with the client
+                while (true) {
+                    if ((clientMsg = reader.readLine()) != null)
+                        System.out.println("\nClient: " + clientMsg);
+                    
+                    System.out.print("-> ");
+                    writer.write(scan.nextLine());
+                    writer.newLine();
+                    writer.flush();
+                }
+            } catch (SocketException se) {
+                System.out.println("Client disconnected.");
             }
-        } catch (Exception BaseException) {
-            BaseException.printStackTrace();
+            catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
